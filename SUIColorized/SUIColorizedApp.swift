b97e68ccccb0c1c2ccc2eac7a07455bd0e75bd91
Danila Kornev <danilakornev92@gmail.com>
2023-06-13 21:46:00 +0300

    Initial Commit

---
+++ SUIColorized/SUIColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIColorizedApp.swift
+//  SUIColorized
+//
+//  Created by Danila Kornev on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUIColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
